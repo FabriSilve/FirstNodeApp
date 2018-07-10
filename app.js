c1b440b7c830526ff3d867252b250d508462c0d7
FabriSilve <fabrizio.silvestri@yahoo.it>
2018-07-10 11:20:28 +0200

    added query parser get data

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const date = require('./date-module');
+const url = require('url');
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -8,8 +9,12 @@ const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   res.write('Hello World - ' + date.myDateTime() + '\n');
-  res.write(req.url);
+  res.write(req.url+'\n');
   console.log(req.url);
+  const q = url.parse(req.url, true).query;
+  const txt = q.year + ' ' + q.month;
+  res.write(txt+'\n');
+  console.log(txt);
   res.end()
 });
 
