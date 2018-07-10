66960dcc342a10ee768a4296d7570388ad56878d
FabriSilve <fabrizio.silvestri@yahoo.it>
2018-07-10 11:26:56 +0200

    added index.html file streaming

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const http = require('http');
 const date = require('./date-module');
 const url = require('url');
+const fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
+  /*res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   res.write('Hello World - ' + date.myDateTime() + '\n');
   res.write(req.url+'\n');
@@ -14,8 +15,11 @@ const server = http.createServer((req, res) => {
   const q = url.parse(req.url, true).query;
   const txt = q.year + ' ' + q.month;
   res.write(txt+'\n');
-  console.log(txt);
-  res.end()
+  console.log(txt);*/
+  fs.readFile('html/index.html', (err, data) => {
+    res.setHeader('Content-Type', 'text/html');
+    res.end(data);
+  })
 });
 
 server.listen(port, hostname, () => {
