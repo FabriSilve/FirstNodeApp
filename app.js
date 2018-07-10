978b10bf7064ca303d3570737fe294e942791986
FabriSilve <fabrizio.silvestri@yahoo.it>
2018-07-10 11:12:42 +0200

    included date-module

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const http = require('http');
+const date = require('./date-module');
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -6,9 +7,9 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end('Hello World\n' + date.myDateTime());
 });
 
 server.listen(port, hostname, () => {
-  console.log('Server running at http://' + hostname + ':' + port + '/');
+  console.log('Server running at http://' + hostname + ':' + port + '/\n' + 'Start: ' + date.myDateTime());
 });
