a8228635c75ba71f0092fb96f42fdb69bc4b6e31
FabriSilve <fabrizio.silvestri@yahoo.it>
2018-07-10 11:55:11 +0200

    added events

--- app.js
+++ app.js
@@ -2,57 +2,22 @@ const http = require('http');
 const date = require('./date-module');
 const url = require('url');
 const fs = require('fs');
+const events = require('events');
 
+
+const eventEmitter = new events.EventEmitter();
+const rs = fs.createReadStream('./data.log');
 const hostname = '127.0.0.1';
 const port = 3000;
 
+rs.on('open', () => { console.log('the file is open');});
+
+const myEventHandler = () => console.log('I hear a scream!');
+eventEmitter.on('scream', myEventHandler);
+eventEmitter.emit('scream');
+
+
 const server = http.createServer((req, res) => {
-  /*res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.write('Hello World - ' + date.myDateTime() + '\n');
-  res.write(req.url+'\n');
-  console.log(req.url);
-  const q = url.parse(req.url, true).query;
-  const txt = q.year + ' ' + q.month;
-  res.write(txt+'\n');
-  console.log(txt);
-
-  fs.appendFile('data.log', 'new visit\n', (err) => {
-    if(err) throw err;
-    console.log('Saved!');
-  });
-  fs.writeFile('lastAccess.log', date.myDateTime(), (err) => {
-    if(err) throw err;
-    console.log('Saved!');
-  });
-  fs.unlink('data.log', (err) => {
-    if(err) throw err;
-    console.log('file deleted');
-  });
-
-  fs.readFile('html/index.html', (err, data) => {
-    res.setHeader('Content-Type', 'text/html');
-    res.end(data);
-  })
-  */
-  /*const q = url.parse(req.url, true);
-  //console.log(q.host);
-  console.log(q.pathname);
-  console.log(q.search);
-  const data = q.query;
-  console.log(data.var);
-  res.end();*/
-
-  const q = url.parse(req.url, true);
-  const filename = './html' + q.pathname;
-  fs.readFile(filename, (err, data) => {
-    if(err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
-    }
-    res.writeHead(200, {'Content-Type':'text/html'});
-    res.end(data);
-  });
 
 });
 
